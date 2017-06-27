ea86028b54dcf2b49b772fa65fa582a53c3d58d2
Tiago Moreira <moreiratiago@hotmail.com>
2017-06-27 16:48:18 +0100

    Dinamic load pages

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -11,9 +11,6 @@ Template7.global = {
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-// Initialize app
-var myApp = new Framework7();
-
 // Init App
 var myApp = new Framework7({
     // Enable Material theme for Android device only
@@ -22,25 +19,57 @@ var myApp = new Framework7({
     template7Pages: true
 });
 
-var view1 = myApp.addView('#view-1');
-var view2 = myApp.addView('#view-2', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+var nearbyView = myApp.addView('#nearby', {
+    domCache: true
 });
-var view3 = myApp.addView('#view-3');
-var view4 = myApp.addView('#view-4');
+var cameraView = myApp.addView('#camera', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true,
+    domCache: true
+});
+var routesView = myApp.addView('#routes', {
+    domCache: true
+});
+var purchasesView = myApp.addView('#purchases', {
+    domCache: true
+});
 
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+
+    var calendarDefault = myApp.calendar({
+        input: '#ks-calendar-default',
+    });
+
+    nearbyView.router.loadPage("views/nearby/home.html");
+    cameraView.router.loadPage("views/camera/home.html");
+    routesView.router.loadPage("views/routes/home.html");
+    purchasesView.router.loadPage("views/purchases/home.html");
 });
 
+
+myApp.onPageInit('nearby/home', function (page) {
+    // Do something here for "about" page
+
+})
+
+myApp.onPageInit('camera/home', function (page) {
+    // Do something here for "about" page
+    console.log("VVVV");
+})
 
 // Now we need to run the code that will be executed only for About page.
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
 myApp.onPageInit('about', function (page) {
     // Do something here for "about" page
+    console.log("XXX");
+})
 
+myApp.onPageInit('index', function (page) {
+    // Do something here for "about" page
+    //view3.router.loadPage()
+    console.log("LLL");
 })
